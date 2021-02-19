bfe79413212cb7bf0ffe80d4833604903e84e378
Daniel Pérez <plupyt_dany@hotmail.com>
2021-02-19 10:53:22 -0600

    components/cart agregado así como sus dependencias

--- components/ui/index.ts
+++ components/ui/index.ts
@@ -1,3 +1,5 @@
 export { default as Grid } from './Grid'
-export { default as Container } from './Container'
 export { default as Logo } from './Logo'
+export { default as Button } from './Button'
+export { default as Container } from './Container'
+export { default as LoadingDots } from './LoadingDots'
