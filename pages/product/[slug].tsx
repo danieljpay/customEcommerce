606023bde4e691968c1408ed4c8441e2dbb50efd
Daniel Pérez <plupyt_dany@hotmail.com>
2021-04-21 10:55:47 -0500

    [slug].tsx ya usa las pages en su footer

--- pages/product/[slug].tsx
+++ pages/product/[slug].tsx
@@ -1,60 +1,42 @@
-// import type {
-//   GetStaticPathsContext,
-//   GetStaticPropsContext,
-//   InferGetStaticPropsType,
-// } from 'next'
+import type { GetStaticPathsContext } from 'next'
 import { useRouter } from 'next/router'
 import { Layout } from '../../components/common'
 import { ProductView } from '../../components/product'
 import { products } from '../../products.json'
 
-// // Data
+export async function getStaticProps() {
+  const { pages } = products
 
-// import { getConfig } from '../../framework/bigcommerce/api'
-// import getProduct from '../../framework/bigcommerce/api/operations/get-product'
-// import getAllPages from '../../framework/bigcommerce/api/operations/get-all-pages'
-// import getAllProductPaths from '../../framework/bigcommerce/api/operations/get-all-product-paths'
+  return {
+    props: { pages },
+    revalidate: 200,
+  }
+}
 
-// export async function getStaticProps({
-//   params,
-//   locale,
-//   preview,
-// }: GetStaticPropsContext<{ slug: string }>) {
-//   const config = getConfig({ locale })
+export async function getStaticPaths({ locales }: GetStaticPathsContext) {
+  const products = []
+  for(let section in products) {
+    //la propiedad cursor es distintiva entre los productos:
+    if(products[section][0].hasOwnProperty('cursor')){ 
+      products[section].forEach(element => {
+        products.concat(element)
+      })
+    }
+  }
 
-//   const { pages } = await getAllPages({ config, preview })
-//   const { product } = await getProduct({
-//     variables: { slug: params!.slug },
-//     config,
-//     preview,
-//   })
-
-//   if (!product) {
-//     throw new Error(`Product with slug '${params!.slug}' not found`)
-//   }
-
-//   return {
-//     props: { pages, product },
-//     revalidate: 200,
-//   }
-// }
-
-// export async function getStaticPaths({ locales }: GetStaticPathsContext) {
-//   const { products } = await getAllProductPaths() //aqui se deben harcodear los productos
-
-//   return {
-//     paths: locales
-//       ? locales.reduce<string[]>((arr, locale) => {
-//           // Add a product path for every locale
-//           products.forEach((product) => {
-//             arr.push(`/${locale}/product${product.node.path}`)
-//           })
-//           return arr
-//         }, [])
-//       : products.map((product) => `/product${product.node.path}`),
-//     fallback: 'blocking',
-//   }
-// }
+  return {
+    paths: locales
+      ? locales.reduce<string[]>((arr, locale) => {
+          // Add a product path for every locale
+          products.forEach((product) => {
+            arr.push(`/${locale}/product${product.node.path}`)
+          })
+          return arr
+        }, [])
+      : products.map((product) => `/product${product.node.path}`),
+    fallback: 'blocking'
+  }
+}
 
 export default function Slug() {
   const router = useRouter()
